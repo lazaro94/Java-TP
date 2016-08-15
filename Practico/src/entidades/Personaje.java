d5cd43c76dad3cd1ffdaa12b99d2a22df3ff4614
Lazaro <Lazaro@192.168.1.2>
2016-08-15 20:04:39 -0300

    Getters y setters

--- Practico/src/entidades/Personaje.java
+++ Practico/src/entidades/Personaje.java
@@ -1,5 +1,55 @@
 package entidades;
 
 public class Personaje {
-
+	
+	private String codigo;
+	private String nombre;
+	private double vida;
+	private double energia;
+	private int defensa;
+	private int evasion;
+	private int ptosTotales;
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getVida() {
+		return vida;
+	}
+	public void setVida(double vida) {
+		this.vida = vida;
+	}
+	public double getEnergia() {
+		return energia;
+	}
+	public void setEnergia(double energia) {
+		this.energia = energia;
+	}
+	public int getDefensa() {
+		return defensa;
+	}
+	public void setDefensa(int defensa) {
+		this.defensa = defensa;
+	}
+	public int getEvasion() {
+		return evasion;
+	}
+	public void setEvasion(int evasion) {
+		this.evasion = evasion;
+	}
+	public int getPtosTotales() {
+		return ptosTotales;
+	}
+	public void setPtosTotales(int ptosTotales) {
+		this.ptosTotales = ptosTotales;
+	}
 }
