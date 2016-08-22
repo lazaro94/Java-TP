30f60e166bb91e10002d32e006fe748aa4e6f11a
Damián <Damin@DESKTOP-UJT67I2>
2016-08-21 21:55:59 -0300

    Modificacion a conexion en servidor.

--- Practico/src/datos/Conexion.java
+++ Practico/src/datos/Conexion.java
@@ -8,10 +8,10 @@ public class Conexion {
 	private Connection con = null;
 	
 	public Connection conectar() throws Exception{
-		String cadena = "jdbc:mysql://localhost/javadb";
+		String cadena = "jdbc:mysql://vps-1039890-x.dattaweb.com/javadb";
         try{
         	Class.forName("com.mysql.jdbc.Driver");         
-            con = DriverManager.getConnection(cadena,"root","root");
+            con = DriverManager.getConnection(cadena,"prueba","123456");
         }
         
         catch(SQLException sqlex){
